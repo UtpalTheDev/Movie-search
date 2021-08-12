6d8b4a9afa85f8f81ad60947ecd40d0d6eb7a76f
Utpal Pati <utpalpati8@gmail.com>
2021-08-12 20:40:13 +0000

    refactor: style add

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,24 @@ import Error from "../assets/Error.svg"
 export function Home() {
   const { data,page,setPage } = useProvider();
 
+  function prevClick(){
+    setPage(prev=>{
+      if(prev !== 1){
+        return prev-1
+      }
+      return prev
+      })
+  }
+  function nextClick(){
+    let status=data.totalResults - page*10
+    console.log("status",status)
+    if(status === 0){
+      return
+    }
+    setPage(prev=>{
+      return prev+1
+    })
+  }
   return (
     <div className="sm:px-2">
       <Search page={page}/>
@@ -13,12 +31,12 @@ export function Home() {
         {data?.length>0 &&
           data.map(movie => {
             return (
-             <div key = {movie.imdbID} className = "sm:flex sm:justify-center w-full h-full"> 
+             <div key = {movie.imdbID} className = "sm:flex sm:justify-center w-full h-full "> 
               <Link to = {`/movie/${movie.imdbID}`}
               className="block w-50 sm:w-60 flex flex-col items-center h-full shadow-md" >
                 <div className="w-full h-full">
                   <img src={ movie.Poster === 'N/A' ? `https://via.placeholder.com/300/fff` : movie.Poster} alt = {movie.Title} className="w-full h-4/5 rounded-t-md" />
-                  <div className="pl-1 sm:text-lg ">{movie.Title}</div>
+                  <div className="pl-1 pt-1 sm:text-lg max-h-14 overflow-hidden">{movie.Title}</div>
                 </div>
               </Link>
              </div> 
@@ -36,21 +54,13 @@ export function Home() {
       { data?.length>0 && <div className="flex justify-center pb-4 pt-2">
         <button 
         className = "bg-red-500 text-white px-2 mr-2 sm:px-3.5 sm:py-2.5"
-        onClick = {()=>{setPage(prev=>{
-          if(prev !== 1){
-            return prev-1
-          }
-          return prev
-          })}}>
+        style={{cursor:page!==1 ? `pointer` : `not-allowed`}}
+        onClick = {prevClick}>
           Prev
         </button>
         <button 
         className = "bg-green-500 px-2 text-white sm:px-3.5 sm:py-2.5"
-        onClick = {()=>{
-          setPage(prev=>{
-            return prev+1
-          })
-        }}>Next</button>
+        onClick = {nextClick}>Next</button>
       </div>
       }
     </div>
